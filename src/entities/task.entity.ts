f0df431ef79493568c7d60476bdb5ab87ea364d2
Sauher <schaueroliver@turr.hu>
2026-03-04 08:13:54 +0100

    Enitiyy

--- src/entities/task.entity.ts
+++ src/entities/task.entity.ts
@@ -1,9 +1,16 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('tasks')
 export class Task {
+  @PrimaryGeneratedColumn()
   id!: number;
+
+  @Column()
   title!: string;
+
+  @Column()
   description!: string;
+
+  @Column({ default: false })
   completed!: boolean;
 }
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,19 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 
 
 
 export class User {
+  @PrimaryGeneratedColumn()
   id!: number;
+  
+  @Column()
   name!: string;
+
+  @Column()
   email!: string;
+
+  @Column()
   password!: string;
 }
